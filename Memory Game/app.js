f311049c38c74456c5c4139e709a938d7271b2d9
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-03 20:51:28 +0330

    Adding card timer to project for hiding cards

--- Memory Game/app.js
+++ Memory Game/app.js
@@ -9,7 +9,7 @@ class Card {
 }
 
 const card1 = new Card('fries','images/fries.png');
-const card2 = new Card('cheesburger','images/cheeseburger.png');
+const card2 = new Card('cheeseburger','images/cheeseburger.png');
 const card3 = new Card('hotdog' , 'images/hotdog.png');
 const card4 = new Card('ice-cream','images/ice-cream.png');
 const card5 = new Card('milkshake','images/milkshake.png');
@@ -38,21 +38,38 @@ for (let i = 12; i > 0; i--) {
     cardArray.splice(randomNumber,1);
 }
 
-const grid = document.getElementById('grid')
+const grid = document.getElementById('grid');
 
-for (let i = 0; i < randomizedArray.length; i++) {
-    let newElement = document.createElement('button');
-    newElement.id = 'card'+ "-" +randomizedArray[i].name;
-    newElement.style.backgroundImage = "url(" + randomizedArray[i].img + ")";
-    if(screen.width>screen.height)
-    {
-        newElement.style.height = "15vh";
-        newElement.style.width = "15vh";
+const startTheGameButton = document.getElementById('startTheGame');
+startTheGameButton.addEventListener('click' ,(e) => {
+    startTheGame();
+});
+
+let Cards = [];
+
+function startTheGame()
+{
+    startTheGameButton.remove();
+    for (let i = 0; i < randomizedArray.length; i++) {
+        let newElement = document.createElement('button');
+        newElement.id = randomizedArray[i].name;
+        newElement.style.backgroundImage = "url(" + randomizedArray[i].img + ")";
+        newElement.style.height = "10vw"
+        newElement.style.width = "10vw"
+        newElement.className= 'cards'
+        Cards.push(newElement);
+        grid.appendChild(newElement);
     }
-    else{
-        newElement.style.height = "15vw"
-        newElement.style.width = "15vw"
-    }
-    grid.appendChild(newElement);
+
+    let timeLeft = 5;
+    let timerId = setInterval(countdown, 1000);
+function countdown() {
+    if (timeLeft == -1) {
+    clearTimeout(timerId);
+    hideCards();
+    } else {
+    timeLeft--;
+    } 
+}
 }
 
